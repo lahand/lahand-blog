93c964e3a20e79b4da2f791fec353a0264e41acd
王帅 <lahand@163.com>
2019-02-28 14:45:37 +0800

    去除警告

--- src/main/java/com/lahand/utils/CodeGenerator.java
+++ src/main/java/com/lahand/utils/CodeGenerator.java
@@ -1,9 +1,7 @@
 package com.lahand.utils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
